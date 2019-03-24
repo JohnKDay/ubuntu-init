0dc22247de2d0f0a54a8e45ddac12fc86ed7d569
John K. Day <johnkday@cisco.com>
2019-03-23 21:57:34 -0700

    Added set_kubeconfig_cluter.rb file and better SAP DH scripts

--- scripts/SAPDH/1-pull-harbor-ca-file.sh
+++ scripts/SAPDH/1-pull-harbor-ca-file.sh
@@ -1,62 +1,74 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# Function: Given CCP CP IP:port address (MGMT_HOST), CCP CP password (PASS), Harbor TC name (CLUSTER) and CCP TC Harbor admin password (HARBOR_PASS)
+#   Will perform:
+#   - Create 'temp' subdirectory
+#   - Obtain session cookie from CCP CP and place in temp directory
+#   - Extract Harbor TC information and place in temp directory
+#   - Download Harbor TC KUBECONFIG file and place in temp directory
+#   - Pull Harbor TC ca.crt file and place in temp directory
+#   - Obtain Harbor TC Load Balancer IP address
+#   - *SUDO* Create /etc/docker/certs.d/<Harbor CP LBIP> directory
+#   - *SUDO* Copy Harbor TC ca.crt file to /etc/docker/certs.d/<Harbor CP LBIP>/
+#   - Pull busybox container image from docker.io
+#   - Login to Harbor registry with admin and password
+#   - Tag busybox image to Harbor IP address in 'library' Project
+#   - Push tagged busybox image to Harbor IP address
 
 set -v
 set -euo pipefail
 
-#CLUSTER=sapdatahub
-CLUSTER=${1:-ccp222b-harbor1}
-HARBOR_PASS=${HARBOR_PASS:-$PASS}
-
-# get cookie
-
+# Verify minimum variables set 
 if [ -z "$MGMT_HOST" ]; then
    printf "Error: need MGMT_HOST env variable set\n"
    exit 1
 fi
+if [ -z "$PASS" ]; then
+   printf "Error: need PASS env veriable set\n"
+   exit 2
+fi
+CLUSTER=${1:-harbor}
+HARBOR_PASS=${HARBOR_PASS:-$PASS}
 
+# Create 'temp' subdirectory
 [ -d temp ] || mkdir temp
 [ -w temp/_context-merge.KUBECONFIG ] || touch temp/_context-merge.KUBECONFIG
 
+# Obtain session cookie from CCP CP and place in temp directory
 curl -k -c temp/cookie.txt -H "Content-Type:application/x-www-form-urlencoded" -d "username=admin&password=${PASS}" https://$MGMT_HOST/2/system/login/
 
-# Get Harbor TC information
-
+# Extract Harbor TC information and place in temp directory
 curl -sk -b temp/cookie.txt https://$MGMT_HOST/2/clusters/${CLUSTER}| tee temp/${CLUSTER}.json| jq '.name,.uuid,.state'
 
-# extract Harbor TC environment file
-
+# Download Harbor TC KUBECONFIG file and place in temp directory
 TC=$(jq -r '.uuid' temp/${CLUSTER}.json)
 echo $TC
+if [ -x "$(type -p set_kubeconfig_cluster.rb)" ]; then
+  curl -sk -b temp/cookie.txt https://$MGMT_HOST/2/clusters/${TC}/env | set_kubeconfig_cluster.rb ${CLUSTER} >  temp/${CLUSTER}.KUBECONFIG
+else
+  curl -sk -b temp/cookie.txt https://$MGMT_HOST/2/clusters/${TC}/env >  temp/${CLUSTER}.KUBECONFIG
+fi
 
-curl -sk -b temp/cookie.txt https://$MGMT_HOST/2/clusters/${TC}/env | set_kubeconfig_cluster.rb ${CLUSTER} >  temp/${CLUSTER}.KUBECONFIG
-
-# write mgmt_host to temp
-
-echo $MGMT_HOST > temp/mgmt_host
-
-# pull harbor CA.crt file to use in other TCs 
-
+# Pull Harbor TC ca.crt file and place in temp directory
 KUBECONFIG=temp/${CLUSTER}.KUBECONFIG kubectl get secret ccp-ingress-tls-ca -n ccp -o jsonpath='{.data.tls\.crt}' |base64 -d | tee  temp/ccp.crt
 
-
-## configure local instance ablilty to push/pull to harbor repo
-
-# Obtain the Harbor Load Balancer IP
-
+# Obtain Harbor TC Load Balancer IP address 
 LBIP=$(KUBECONFIG=temp/${CLUSTER}.KUBECONFIG kubectl get svc -n ccp -o jsonpath='{.items..status.loadBalancer.ingress[0].ip}') 
 
 # Copy Harbor CA cert into /etc/docker/certs.d for use on install machine
-
 sudo mkdir -p /etc/docker/certs.d/${LBIP}
-
 sudo cp temp/ccp.crt /etc/docker/certs.d/${LBIP}/ca.crt
 
-docker pull johnkday/ubuntu-s3
+# Pull busybox container image from docker.io
+docker pull busybox
 
+#Login to Harbor registry with admin and password
 docker login -u admin -p ${HARBOR_PASS} ${LBIP}
 
-docker tag johnkday/ubuntu-s3 ${LBIP}/library/ubuntu-s3
+# Tag busybox image to Harbor IP address in 'library' Project
+docker tag busybox ${LBIP}/library/busybox
 
-docker push ${LBIP}/library/ubuntu-s3
+# Push tagged busybox image to Harbor IP address
+docker push ${LBIP}/library/busybox
 
-
+exit 0
--- scripts/SAPDH/2-pull-CCP-k8s-KUBECONFIG.sh
+++ scripts/SAPDH/2-pull-CCP-k8s-KUBECONFIG.sh
@@ -1,37 +1,52 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# Function: Given CCP CP IP:port address (MGMT_HOST), CCP CP password (PASS), SAP Data Hub TC name ($1)
+#   Will perform:
+#   - Create 'temp' subdirectory
+#   - Obtain session cookie from CCP CP and place in temp directory
+#   - Extract SAP Data Hub TC information and place in temp directory
+#   - Download SAP Data Hub TC KUBECONFIG file and place in temp directory
+#   - Test the KUBECONFIG file
+#   - Print export value to reference the KUBECONFIG file
+
 set -v
+set -euo pipefail
 
-#CLUSTER=sapdatahub
-CLUSTER=${1:-ccp222b-cluster1o}
-
-# get cookie
-
+# Verify minimum variables set 
 if [ -z "$MGMT_HOST" ]; then
    printf "Error: need MGMT_HOST env variable set\n"
    exit 1
 fi
+if [ -z "$PASS" ]; then
+   printf "Error: need PASS env veriable set\n"
+   exit 2
+fi
+CLUSTER=${1:-datahub}
 
+# Create 'temp' subdirectory
 [ -d temp ] || mkdir temp
 [ -w temp/_context-merge.KUBECONFIG ] || touch temp/_context-merge.KUBECONFIG
 
+# Obtain session cookie from CCP CP and place in temp directory
 curl -k -c temp/cookie.txt -H "Content-Type:application/x-www-form-urlencoded" -d "username=admin&password=${PASS}" https://$MGMT_HOST/2/system/login/
 
-# get SAP DH TC information 
-
+# Extract SAP Data Hub TC information and place in temp directory
 curl -sk -b temp/cookie.txt https://$MGMT_HOST/2/clusters/${CLUSTER}| tee temp/${CLUSTER}.json| jq '.name,.uuid,.state'
-# get TC environment file
 
+# Download SAP Data Hub TC KUBECONFIG file and place in temp directory
 TC=$(jq -r '.uuid' temp/${CLUSTER}.json)
 echo $TC
+if [ -x "$(type -p set_kubeconfig_cluster.rb)" ]; then
+  curl -sk -b temp/cookie.txt https://$MGMT_HOST/2/clusters/${TC}/env | set_kubeconfig_cluster.rb ${CLUSTER} >  temp/${CLUSTER}.KUBECONFIG
+else
+  curl -sk -b temp/cookie.txt https://$MGMT_HOST/2/clusters/${TC}/env >  temp/${CLUSTER}.KUBECONFIG
+fi
 
-curl -sk -b temp/cookie.txt https://$MGMT_HOST/2/clusters/${TC}/env | set_kubeconfig_cluster.rb ${CLUSTER} >  temp/${CLUSTER}.KUBECONFIG
-
-# test kubctl commands
-
+# Test the KUBECONFIG file
 KUBECONFIG=temp/${CLUSTER}.KUBECONFIG kubectl get nodes -o wide
 
-# export the value to set KUBECONFIG
-
+# Print the value to set KUBECONFIG for use in environment
+echo "Paste the below line into your shell environment"
 echo "export KUBECONFIG=${PWD}/temp/${CLUSTER}.KUBECONFIG"
 
-
+exit 0 
